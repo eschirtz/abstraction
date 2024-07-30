406a50d8978d5daf8ad6b1bd0812fb326f295f6d
eschirtz <eschirtzinger@gmail.com>
2024-07-29 22:09:58 -0700

    dilly dallying

--- src/views/GameSplash.vue
+++ src/views/GameSplash.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="bg">
     <h1 class="title">FRUITAL SURVIVAL</h1>
+    <img class="watermelon" :src="watermelonGIF" alt="watermelon GIF">
+    <img class="banana" :src="bananaGIF" alt="banana GIF">
     <img class="fire" :src="fireGIF" alt="animated fire GIF">
   </div>
 </template>
 
 <script lang="ts" setup>
 
+const bananaGIF = "/img/banana.gif";
+const watermelonGIF = "/img/watermelon.gif"
 const fireGIF = "/img/fire.gif";
 </script>
 
@@ -35,5 +39,27 @@ const fireGIF = "/img/fire.gif";
   position: absolute;
   width: 100%;
   bottom: 0;
+}
+
+.watermelon {
+  width: 210px;
+  height: 210px;
+  top: 160px;
+  left: 15%;  
+  transform: rotate(-25deg);
+  position: absolute;
+  object-fit: contain;
+  z-index: 2;
+}
+
+.banana {
+  width: 200px;
+  height: 200px;
+  top: 100px;
+  right: 20%;  
+  transform: rotate(-25deg);
+  position: absolute;
+  object-fit: contain;
+  z-index: 2;
 }
 </style>
